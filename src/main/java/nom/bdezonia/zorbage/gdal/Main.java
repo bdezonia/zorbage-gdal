7f15026b25bff337d789cea0dd531126ce0926fa
Barry DeZonia <bdezonia@gmail.com>
2020-06-12 23:06:27 -0500

    Add a bunch of links to sample datasets

--- src/main/java/nom/bdezonia/zorbage/gdal/Main.java
+++ src/main/java/nom/bdezonia/zorbage/gdal/Main.java
@@ -40,7 +40,39 @@ public class Main {
 
 		//String filename = "/home/bdz/images/modis/modis.hdf";
 		//String filename = "/home/bdz/images/qesdi/cru_v3_dtr_clim10.nc";
-		String filename = "/home/bdz/images/imagej-samples/clown.jpg";
+		
+		//String filename = "/home/bdz/images/imagej-samples/AuPbSn40.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/bat-cochlea-renderings.tif";
+		//String filename = "/home/bdz/images/imagej-samples/bat-cochlea-volume.tif";
+		//String filename = "/home/bdz/images/imagej-samples/blobs.gif";
+		//String filename = "/home/bdz/images/imagej-samples/boats.gif";
+		//String filename = "/home/bdz/images/imagej-samples/bridge.gif";
+		// dicom not supported in gdal
+		//String filename = "/home/bdz/images/imagej-samples/Cardio.dcm";
+		//String filename = "/home/bdz/images/imagej-samples/Cell_Colony.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/clown.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/confocal-series.tif";
+		// dicom not supported in gdal
+		//String filename = "/home/bdz/images/imagej-samples/CT.dcm";
+		//String filename = "/home/bdz/images/imagej-samples/Dot_Blot.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/embryos.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/FluorescentCells.tif";
+		//String filename = "/home/bdz/images/imagej-samples/flybrain.tif";
+		//String filename = "/home/bdz/images/imagej-samples/gel.gif";
+		//String filename = "/home/bdz/images/imagej-samples/hela-cells.tif";
+		//String filename = "/home/bdz/images/imagej-samples/leaf.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/LineGraph.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/m51.tif";
+		//String filename = "/home/bdz/images/imagej-samples/mri-stack.tif";
+		//String filename = "/home/bdz/images/imagej-samples/NileBend.jpg";
+		//String filename = "/home/bdz/images/imagej-samples/organ-of-corti.tif";
+		//String filename = "/home/bdz/images/imagej-samples/particles.gif";
+		//String filename = "/home/bdz/images/imagej-samples/Rat_Hippocampal_Neuron.tif";
+		//String filename = "/home/bdz/images/imagej-samples/mitosis.tif";
+		//String filename = "/home/bdz/images/imagej-samples/t1-head.tif";
+		//String filename = "/home/bdz/images/imagej-samples/t1-rendering.tif";
+		//String filename = "/home/bdz/images/imagej-samples/TEM_filter_sample.jpg";
+		String filename = "/home/bdz/images/imagej-samples/Tree_Rings.jpg";
 
 		DataBundle bundle = Gdal.open(filename);
 		
