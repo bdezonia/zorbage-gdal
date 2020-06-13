52779de6330c0a10e38277c1616a209f5b3c2f5d
Barry DeZonia <bdezonia@gmail.com>
2020-06-12 22:29:28 -0500

    Improve data reading from gdal
    The code currently supports standard 2d/3d images. It does not
    yet support datasets that have multiple subdatasets.

--- src/main/java/nom/bdezonia/zorbage/gdal/Gdal.java
+++ src/main/java/nom/bdezonia/zorbage/gdal/Gdal.java
@@ -26,10 +26,23 @@
  */
 package nom.bdezonia.zorbage.gdal;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.gdal.gdal.Band;
+import org.gdal.gdal.Dataset;
+import org.gdal.gdal.gdal;
+import org.gdal.gdalconst.gdalconst;
+
+import nom.bdezonia.zorbage.algebra.Allocatable;
+import nom.bdezonia.zorbage.algebra.G;
 import nom.bdezonia.zorbage.multidim.MultiDimDataSource;
+import nom.bdezonia.zorbage.multidim.MultiDimStorage;
+import nom.bdezonia.zorbage.procedure.Procedure4;
+import nom.bdezonia.zorbage.sampling.IntegerIndex;
+import nom.bdezonia.zorbage.sampling.SamplingCartesianIntegerGrid;
+import nom.bdezonia.zorbage.sampling.SamplingIterator;
 import nom.bdezonia.zorbage.type.float32.complex.ComplexFloat32Member;
 import nom.bdezonia.zorbage.type.float32.real.Float32Member;
 import nom.bdezonia.zorbage.type.float64.complex.ComplexFloat64Member;
@@ -61,6 +74,264 @@ public class Gdal {
 		public List<MultiDimDataSource<Float64Member>> doubles;
 		public List<MultiDimDataSource<ComplexFloat32Member>> cfloats;  // including c16int
 		public List<MultiDimDataSource<ComplexFloat64Member>> cdoubles; // including c32int
+	}
+
+	/**
+	 * This must be called once at startup by users of this gdal interface package
+	 */
+	public static void init() {
+		gdal.AllRegister();
+	}
+	
+	/**
+	 * 
+	 * @param filename
+	 */
+	public static DataBundle open(String filename) {
+
+		DataBundle resultSets = new DataBundle();
+		Dataset ds = gdal.Open(filename);
+		System.out.println(ds.GetDescription());
+		if (ds.GetRasterCount() == 0) {
+			// this file probably contains a bunch of subdatasets. somehow query what they are
+			// and use gdal.Open() on each of them.
+			System.out.println("this dataset has been encoded as a bunch of subdatasets");
+		}
+		else {
+			// report the dataset's info
+			System.out.println(" x size " + ds.GetRasterXSize());
+			System.out.println(" y size " + ds.GetRasterYSize());
+			System.out.println(" rasters " + ds.GetRasterCount());
+			int type = -1;
+			int xSize = ds.GetRasterXSize();
+			int ySize = ds.getRasterYSize();
+			for (int i = 1; i <= ds.GetRasterCount(); i++) {
+				Band band = ds.GetRasterBand(i);
+				if (type == -1)
+					type = band.GetRasterDataType();
+				if (band.GetRasterDataType() != type)
+					throw new IllegalArgumentException("data has multiple different band types!");
+				if ((band.GetXSize() != xSize) || (band.GetYSize() != ySize))
+					throw new IllegalArgumentException("data has multiple band resolutions!");
+				System.out.println("   band " + i + " " + type + " " + band.GetDescription());
+			}
+			if (type == gdalconst.GDT_Byte)
+				resultSets.uint8s = Arrays.asList(loadUByteData(ds, G.UINT8.construct()));
+			else if (type == gdalconst.GDT_UInt16)
+				resultSets.uint16s = Arrays.asList(loadUShortData(ds, G.UINT16.construct()));
+			else if (type == gdalconst.GDT_Int16)
+				resultSets.int16s = Arrays.asList(loadShortData(ds, G.INT16.construct()));
+			else if (type == gdalconst.GDT_UInt32)
+				resultSets.uint32s = Arrays.asList(loadUIntData(ds, G.UINT32.construct()));
+			else if (type == gdalconst.GDT_Int32)
+				resultSets.int32s = Arrays.asList(loadIntData(ds, G.INT32.construct()));
+			else if (type == gdalconst.GDT_Float32)
+				resultSets.floats = Arrays.asList(loadFloatData(ds, G.FLT.construct()));
+			else if (type == gdalconst.GDT_Float64)
+				resultSets.doubles = Arrays.asList(loadDoubleData(ds, G.DBL.construct()));
+			else if (type == gdalconst.GDT_CInt16)
+				// I have no exact match for this class: widen data
+				resultSets.cfloats = Arrays.asList(loadComplexFloatData(ds, G.CFLT.construct()));
+			else if (type == gdalconst.GDT_CInt32)
+				// I have no exact match for this class: widen data
+				resultSets.cdoubles = Arrays.asList(loadComplexDoubleData(ds, G.CDBL.construct()));
+			else if (type == gdalconst.GDT_CFloat32)
+				resultSets.cfloats = Arrays.asList(loadComplexFloatData(ds, G.CFLT.construct()));
+			else if (type == gdalconst.GDT_CFloat64)
+				resultSets.cdoubles = Arrays.asList(loadComplexDoubleData(ds, G.CDBL.construct()));
+			else
+				System.out.println("Ignoring unknown data type "+gdal.GetDataTypeName(type));
+			System.out.println("data loaded");
+		}
+		return resultSets;
+	}
+	
+	private static <U extends Allocatable<U>>
+		MultiDimDataSource<U> loadData(Dataset ds, U type, Procedure4<Band, Integer, Integer, U> proc)
+	{
+		int planes = ds.getRasterCount();
+		long[] dims;
+		if (planes == 1) {
+			dims = new long[] {ds.getRasterXSize(), ds.GetRasterYSize()};
+		}
+		else {
+			dims = new long[] {ds.getRasterXSize(), ds.GetRasterYSize(), planes};
+		}
+		MultiDimDataSource<U> data = MultiDimStorage.allocate(dims, type);
+		
+		long[] minPt = new long[data.numDimensions()];
+		long[] maxPt = new long[data.numDimensions()];
+		for (int i = 0; i < data.numDimensions(); i++) {
+			maxPt[i] = data.dimension(i) - 1;
+		}
+		for (int i = 0; i < planes; i++) {
+			if (planes > 1) {
+				minPt[2] = i;
+				maxPt[2] = i;
+			}
+			Band band = ds.GetRasterBand(i+1);
+			SamplingCartesianIntegerGrid grid = new SamplingCartesianIntegerGrid(minPt, maxPt);
+			if (planes > 1) {
+				minPt[2] = 0;
+				maxPt[2] = data.dimension(i)-1;
+			}
+			SamplingIterator<IntegerIndex> iter = grid.iterator();
+			IntegerIndex index = new IntegerIndex(data.numDimensions());
+			for (int y = 0; y < ds.GetRasterYSize(); y++) {
+				for (int x = 0; x < ds.GetRasterXSize(); x++) {
+					iter.next(index);
+					proc.call(band, x, y, type);
+					data.set(index, type);
+				}				
+			}
+		}
+		return data;
+	}
+
+	private static MultiDimDataSource<UnsignedInt8Member> loadUByteData(Dataset ds, UnsignedInt8Member type) {
+		Procedure4<Band,Integer,Integer,UnsignedInt8Member> proc =
+				new Procedure4<Band, Integer, Integer, UnsignedInt8Member>()
+		{
+			private byte[] buffer = new byte[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, UnsignedInt8Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<UnsignedInt16Member> loadUShortData(Dataset ds, UnsignedInt16Member type) {
+		Procedure4<Band,Integer,Integer,UnsignedInt16Member> proc =
+				new Procedure4<Band, Integer, Integer, UnsignedInt16Member>()
+		{
+			private short[] buffer = new short[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, UnsignedInt16Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<SignedInt16Member> loadShortData(Dataset ds, SignedInt16Member type) {
+		Procedure4<Band,Integer,Integer,SignedInt16Member> proc =
+				new Procedure4<Band, Integer, Integer, SignedInt16Member>()
+		{
+			private short[] buffer = new short[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, SignedInt16Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<UnsignedInt32Member> loadUIntData(Dataset ds, UnsignedInt32Member type) {
+		Procedure4<Band,Integer,Integer,UnsignedInt32Member> proc =
+				new Procedure4<Band, Integer, Integer, UnsignedInt32Member>()
+		{
+			private int[] buffer = new int[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, UnsignedInt32Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<SignedInt32Member> loadIntData(Dataset ds, SignedInt32Member type) {
+		Procedure4<Band,Integer,Integer,SignedInt32Member> proc =
+				new Procedure4<Band, Integer, Integer, SignedInt32Member>()
+		{
+			private int[] buffer = new int[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, SignedInt32Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<Float32Member> loadFloatData(Dataset ds, Float32Member type) {
+		Procedure4<Band,Integer,Integer,Float32Member> proc =
+				new Procedure4<Band, Integer, Integer, Float32Member>()
+		{
+			private float[] buffer = new float[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, Float32Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<Float64Member> loadDoubleData(Dataset ds, Float64Member type) {
+		Procedure4<Band,Integer,Integer,Float64Member> proc =
+				new Procedure4<Band, Integer, Integer, Float64Member>()
+		{
+			private double[] buffer = new double[1];
+			@Override
+			public void call(Band band, Integer x, Integer y, Float64Member type) {
+				band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), buffer, 0, 0);
+				type.setV(buffer[0]);
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<ComplexFloat32Member> loadComplexFloatData(Dataset ds, ComplexFloat32Member type) {
+		Procedure4<Band,Integer,Integer,ComplexFloat32Member> proc =
+				new Procedure4<Band, Integer, Integer, ComplexFloat32Member>()
+		{
+			private float[] fbuffer = new float[2];
+			private short[] sbuffer = new short[2];
+
+			@Override
+			public void call(Band band, Integer x, Integer y, ComplexFloat32Member type) {
+				if (band.getDataType() == gdalconst.GDT_CInt16) {
+					band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), sbuffer, 0, 0);
+					type.setR(sbuffer[0]);
+					type.setI(sbuffer[1]);
+				}
+				else {  // data type = GDT_CFloat32
+					band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), fbuffer, 0, 0);
+					type.setR(fbuffer[0]);
+					type.setI(fbuffer[1]);
+				}
+			}
+		};
+		return loadData(ds, type, proc);
+	}
+
+	private static MultiDimDataSource<ComplexFloat64Member> loadComplexDoubleData(Dataset ds, ComplexFloat64Member type) {
+		Procedure4<Band,Integer,Integer,ComplexFloat64Member> proc =
+				new Procedure4<Band, Integer, Integer, ComplexFloat64Member>()
+		{
+			private double[] dbuffer = new double[2];
+			private int[] ibuffer = new int[2];
+
+			@Override
+			public void call(Band band, Integer x, Integer y, ComplexFloat64Member type) {
+				if (band.getDataType() == gdalconst.GDT_CInt32) {
+					band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), ibuffer, 0, 0);
+					type.setR(ibuffer[0]);
+					type.setI(ibuffer[1]);
+				}
+				else {  // data type = GDT_CFloat64
+					band.ReadRaster(x, y, 1, 1, 1, 1, band.getDataType(), dbuffer, 0, 0);
+					type.setR(dbuffer[0]);
+					type.setI(dbuffer[1]);
+				}
+			}
+		};
+		return loadData(ds, type, proc);
 	}
 	
 }
--- src/main/java/nom/bdezonia/zorbage/gdal/Main.java
+++ src/main/java/nom/bdezonia/zorbage/gdal/Main.java
@@ -1,31 +1,58 @@
+/*
+ * zorbage-gdal: code for loading gdal files into zorbage data structures for further processing
+ *
+ * Copyright (C) 2020 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package nom.bdezonia.zorbage.gdal;
 
-import org.gdal.gdal.Band;
-import org.gdal.gdal.Dataset;
-import org.gdal.gdal.gdal;
-import org.gdal.ogr.Layer;
+import nom.bdezonia.zorbage.gdal.Gdal.DataBundle;
 
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		String filename = "/home/bdz/Downloads/modis.hdf";
-	    gdal.AllRegister();
-	    Dataset ds = gdal.Open(filename);
-		System.out.println(ds.GetDescription());
-		System.out.println(" x size " + ds.GetRasterXSize());
-		System.out.println(" y size " + ds.GetRasterYSize());
-		System.out.println(" layers " + ds.GetLayerCount());
-		for (int i = 0; i < ds.GetLayerCount(); i++) {
-			Layer layer = ds.GetLayer(i);
-			System.out.println("   layer " + i + " " + layer.GetName());
-		}
-		System.out.println(" rasters " + ds.GetRasterCount());
-		for (int i = 0; i < ds.GetRasterCount(); i++) {
-			Band band = ds.GetRasterBand(i);
-			System.out.println("   band " + i + " " + band.GetRasterDataType() + " " + band.GetDescription());
-		}
-		Band band = ds.GetRasterBand(0);
-		System.out.println(band.getDataType());
-		//ds.ReadRaster(xoff, yoff, xsize, ysize, buf_xsize, buf_ysize, buf_type, regularArrayOut, band_list, nPixelSpace)
+		Gdal.init();
+
+		//String filename = "/home/bdz/images/modis/modis.hdf";
+		//String filename = "/home/bdz/images/qesdi/cru_v3_dtr_clim10.nc";
+		String filename = "/home/bdz/images/imagej-samples/clown.jpg";
+
+		DataBundle bundle = Gdal.open(filename);
+		
+		if (bundle.uint8s != null) System.out.println("one or more ubyte datasets were loaded");
+		if (bundle.uint16s != null) System.out.println("one or more ushort datasets were loaded");
+		if (bundle.int16s != null) System.out.println("one or more short datasets were loaded");
+		if (bundle.uint32s != null) System.out.println("one or more uint datasets were loaded");
+		if (bundle.int32s != null) System.out.println("one or more int datasets were loaded");
+		if (bundle.floats != null) System.out.println("one or more float datasets were loaded");
+		if (bundle.doubles != null) System.out.println("one or more double datasets were loaded");
+		if (bundle.cfloats != null) System.out.println("one or more complex float datasets were loaded");
+		if (bundle.cdoubles != null) System.out.println("one or more complex double datasets were loaded");
+		if (bundle.chars != null) System.out.println("some character data was loaded");
 	}
 }
